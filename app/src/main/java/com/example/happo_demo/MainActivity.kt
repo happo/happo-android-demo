23dc0bc4a665b648c52c75403e3a38f1be16cbe9
Henric Trotzig <henric.trotzig@gmail.com>
2020-09-25 12:41:07 +0200

    Take screenshot and upload to s3
    This commit is a POC of taking a screenshot of the UI and then sending
    that screenshot to S3. A secrets.properties (not in the git index) file
    contains the api keys necessary to talk to aws.

--- app/src/main/java/com/example/happo_demo/MainActivity.kt
+++ app/src/main/java/com/example/happo_demo/MainActivity.kt
@@ -1,24 +1,41 @@
 package com.example.happo_demo
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat
 import android.graphics.Color
+import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.drawToBitmap
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.ObjectMetadata
+import org.jetbrains.anko.doAsync
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var main: View
-    private lateinit var label: View
+  private lateinit var main: View
+  private lateinit var label: View
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        main = findViewById(R.id.main)
-        label = findViewById(R.id.label)
-        main.post {
-            val b = main.drawToBitmap()
-            main.setBackgroundColor(Color.parseColor("#999999"))
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    main = findViewById(R.id.main)
+    label = findViewById(R.id.label)
+    main.post {
+      doAsync {
+        val bitmap = main.drawToBitmap()
+        val outStream = ByteArrayOutputStream()
+        bitmap.compress(CompressFormat.PNG, 0 /*ignored for PNG*/, outStream)
+        val inStream = ByteArrayInputStream(outStream.toByteArray())
+        val client =
+          AmazonS3Client(BasicAWSCredentials(BuildConfig.S3_ACCESS_KEY, BuildConfig.S3_SECRET_KEY))
+        val metadata = ObjectMetadata()
+        metadata.contentType = "image/png"
+        client.putObject("happo-android-demo", "happo-test.png", inStream, metadata)
+        main.setBackgroundColor(Color.parseColor("#999999"))
+      }
     }
+  }
 }
